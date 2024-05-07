54ed11ae0c9e90233192ff73739bae485c8ab6fd
MartinPettersen <searchingtheskyfor@gmail.com>
2024-05-07 12:26:56 +0200

    setup project

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='min-w-screen h-20 sticky top-0 bg-slate-700 flex items-center justify-center font-bold text-5xl text-white'>Matvare Priser</div>
+  )
+}
+
+export default Navbar
